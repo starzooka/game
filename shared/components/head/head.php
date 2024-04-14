c5e22673417c061a0e17a4c6f8cc5fe633f388f8
ZYRACX <soumya.lodh206@gmail.com>
2024-04-14 13:51:40 +0530

    fix: Refactor CSS for body and sidebar, and update stylesheet path in head.php

--- shared/components/head/head.php
+++ shared/components/head/head.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="./css/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
